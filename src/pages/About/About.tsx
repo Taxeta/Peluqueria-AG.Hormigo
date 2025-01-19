86f90613ca78f876d46eec1dc0ebd34dc03abc17
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2025-01-19 18:46:00 +0100

    Refactroring style and sizes

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -11,9 +11,26 @@ const About = () => {
         alt="tarjeta empresa con logo"
       />
       <div className="horario-container">
-        <span className="info-horario">Horario de apertura</span>
+        <h3 className="info-horario">Horario de apertura</h3>
         <span className="info-horario">Lunes-Sábado 9:00 - 14:00</span>
-        <span className="info-horario">Excluidos Domingos y Festivos</span>
+        <span className="info-horario">Cerrado Domingos y Festivos</span>
+      </div>
+      <div className="telefono-container">
+        <h3 className="info-telefono">Reserva de horas</h3>
+        <h4 className="info-telefono">Llamar</h4>
+        <div>
+          <i className="fa-duotone fa-solid fa-phone fa-2x contacto-iconos"></i>
+          <a href="tel:+34722616550" className="info-telefono">
+            +34 722 61 65 50
+          </a>
+        </div>
+        <h4 className="info-telefono">Whatsapp</h4>
+        <div>
+          <i className="fa-brands fa-square-whatsapp fa-2x contacto-iconos"></i>
+          <a href="https://wa.link/ka47dw" className="info-telefono ml-">
+            +34 722 61 65 50
+          </a>
+        </div>
       </div>
       <p className="info-pharagraph">
         ¡Descubre la diferencia de un cuidado capilar excepcional con nosotros!
